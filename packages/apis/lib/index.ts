e4ac45e063fc84c984a574bb5af13c6236e998af
warsam-e <w.egeh00@gmail.com>
2025-01-07 18:57:20 -0500

    fix: remove panda from apis index

--- packages/apis/lib/index.ts
+++ packages/apis/lib/index.ts
@@ -1,7 +1,6 @@
 export * as anilist from './anilist';
 export * as fun from './fun';
 export * as images from './images';
-export * as panda from './panda';
 export * as trace from './trace';
 export * as zerochan from './zerochan';
 
