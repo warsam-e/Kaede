0a4e052e2d1d37909c7c018229027f720a60125f
warsam-e <w.egeh00@gmail.com>
2025-09-01 12:09:22 -0400

    fix: docker health check

--- packages/bot/lib/server/index.ts
+++ packages/bot/lib/server/index.ts
@@ -1,17 +1,19 @@
-import { get_buf } from '@kaede/utils';
+import { get_buf, get_env, IS_PROD } from '@kaede/utils';
 import { Elysia } from 'elysia';
 import { bot } from '../bot';
 
-const API_PORT = 3005;
+const API_PORT = IS_PROD ? 3000 : get_env('API_PORT', 'number');
 
 export async function init_server() {
 	new Elysia().use(routes).listen(API_PORT, () => console.log(`Listening on port ${API_PORT}`));
 }
 
-const routes = new Elysia().get('/', async ({ set }) => {
-	const buf = await get_buf(
-		bot.user?.avatarURL({ extension: bot.user.avatar?.startsWith('a_') ? 'gif' : 'png' }) ?? '',
-	);
-	set.headers['Content-Type'] = 'image/png';
-	return buf;
-});
+const routes = new Elysia()
+	.get('/', async ({ set }) => {
+		const buf = await get_buf(
+			bot.user?.avatarURL({ extension: bot.user.avatar?.startsWith('a_') ? 'gif' : 'png' }) ?? '',
+		);
+		set.headers['Content-Type'] = 'image/png';
+		return buf;
+	})
+	.get('/health', { status: 'ok' });
